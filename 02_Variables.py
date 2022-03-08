719149a7360f7817304b665a21febb6c90a73066
Pizzachomper <100880007+Pizzachomper@users.noreply.github.com>
2022-03-08 14:49:33 +1300

    Add files via upload

---
+++ 02_Variables.py
@@ -0,0 +1,17 @@
+# code to learn about variables
+# variables can be a string
+greeting = "hello world"
+# they can also be numbers
+fav_num = 7
+# this variable has a problem
+my_name = "Pizzachomper"
+
+print(greeting)
+print(fav_num * 2)
+
+num_string = "5"
+print(num_string * 5)
+
+# below is a constant (the name is in UPPERCASE)
+PI = 3.14
+num_2 = 2
